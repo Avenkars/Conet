75814ddccf349df6dcc1e9f7979e799bb623b3a5
Avenkars <138456303+Avenkars@users.noreply.github.com>
2023-11-11 13:29:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from func.Conet import *
+from func.Conet import Conet
 import tkinter as tk
 from PIL import ImageTk
 
@@ -7,7 +7,7 @@ _: int = 0
 
 # 创建窗口
 window = tk.Tk()
-window.resizable(False, False)
+window.resizable(_, _)
 window.title('A.U.A for {6-4}')
 
 
@@ -25,7 +25,7 @@ class label():
 # 定义显示方法
 def show():
     label.bgLabel.pack()
-    label.settings_imgBtn.place(x=655, y=2)
+    label.settings_imgBtn.place(x=660, y=1)
 
 
 # 调用显示方法
